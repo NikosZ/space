01daa57606f276f2c350233d26f450733e2c4618
Owner <nick.zarifis@hotmail.com>
2013-07-09 19:56:53 +0300

    string.cpp

---
+++ string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int main()
+{
+	char t[3],p[5];
+	gets(t);
+	gets(p);
+	cout<<t<<endl; //ERROR 
+	cout<<p<<endl; //ERROR
+	return 0;
+}
+//DOSE ISODO AKROPOLIS \n Arxigos
